b4eec42b4f2d970ea17e7d8b85fca1094a55966a
Gorka97 <gorkaar@hotmail.com>
2017-10-24 19:34:29 +0200

    Update insertarPregunta.php

--- ProyectoSW-HTML5-imagen/insertarPregunta.php
+++ ProyectoSW-HTML5-imagen/insertarPregunta.php
@@ -6,10 +6,10 @@ function goBack() {
 </script> 
 </HTML>
 <?php
-	$link = mysqli_connect("localhost","root","root","quiz");
+	$link = mysqli_connect("localhost","id2956013_root","********","id2956013_quiz");
 	$imagen = $_FILES['imagen']['name'];
 	if ($_FILES["imagen"]["tmp_name"]!=NULL){
-		$carpeta = $_SERVER['DOCUMENT_ROOT'].'ProyectoSW-HTML5-imagen/fotos/';
+		$carpeta = $_SERVER['DOCUMENT_ROOT'].'/ProyectoSW-HTML5-imagen/ProyectoSW-HTML5-imagen/fotos/';
 		move_uploaded_file($_FILES["imagen"]["tmp_name"],$carpeta.$imagen);
 	}
 	$sql="INSERT INTO preguntas(email,pregunta,res_correcta,res_incorrecta1,res_incorrecta2,res_incorrecta3,dificultad,tema,imagen) VALUES ('$_POST[email]','$_POST[pregunta]','$_POST[res_correcta]','$_POST[res_incorrecta1]','$_POST[res_incorrecta2]','$_POST[res_incorrecta3]','$_POST[dificultad]','$_POST[tema]','$imagen')";
